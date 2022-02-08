630bccd501320427f259e6bf12a21877650142af
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-07 22:32:06 -0600

    post tests done

--- api/server.js
+++ api/server.js
@@ -3,6 +3,31 @@ const express = require('express')
 const User = require('./users/model')
 
 const server = express()
+server.use(express.json())
+
+////POST////------------------------------
+
+server.post('/api/users', (req, res) => {
+  const user = req.body;
+  if(!user.name || !user.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user"
+    })
+  } else {
+    User.insert(user)
+      .then(newUser => {
+        res.status(201).json(newUser)
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "There was an error while saving the user to the database",
+          error: err.message
+        })
+      })
+  }
+})
+
+////GET////------------------------------
 
 server.get('/api/users', (req, res) => {
   User.find()
