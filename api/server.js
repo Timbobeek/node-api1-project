6f48041bba3d4e033a1ae7f4ee28498f11f6b5e7
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-07 21:38:43 -0600

    first tests passed

--- api/server.js
+++ api/server.js
@@ -1,5 +1,27 @@
-// BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express')
+const User = require('./users/model')
 
-////// start here
+const server = express()
+
+server.get('/api/users', (req, res) => {
+  User.find()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "The users information could not be retrieved",
+        error: err.message
+      })
+    })
+})
+
+server.use('*', (req, res)=>{
+  res.status(404).json({
+    message: 'not found'
+  })
+})
+
+module.exports = server; 
+
