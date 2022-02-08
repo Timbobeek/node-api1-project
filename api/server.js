942cf764c0a46dec8190e7130cb14a1238808c01
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-07 23:39:12 -0600

    mvp done

--- api/server.js
+++ api/server.js
@@ -81,13 +81,31 @@ server.delete('/api/users/:id', async (req, res) => {
   }
 })
 
+////PUT////------------------------------
 
-
-
-
-
-
-
+server.put('/api/users/:id', async (req, res) =>{
+  try {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist"
+      })
+    } else {
+      if (!req.body.name || !req.body.bio){
+        res.status(400).json({
+          message: "Please provide name and bio for the user"
+        })
+      } else {
+        const modifiedUser = await User.update(req.params.id, req.body)
+        res.status(200).json(modifiedUser)
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user information could not be modified"
+    })
+  }
+})
 
 server.use('*', (req, res)=>{
   res.status(404).json({
